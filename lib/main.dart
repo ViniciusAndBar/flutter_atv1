23633607aa774d53c3db81d1161df76071a8338f
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-11-16 10:45:29 -0300

    Alteração

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shared Preferences Demo',
+      title: 'Shared Preferences',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
